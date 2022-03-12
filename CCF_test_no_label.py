ae22dc8aba1e2d64b865855e17b6321215d1a02c
JunhuiLiu <1948009725@qq.com>
2022-03-12 20:05:09 +0800

    CCF using fits name (fits name is one fits)

--- CCF_test_no_label.py
+++ CCF_test_no_label.py
@@ -6,7 +6,43 @@ import os
 from laspec import mrs
 
 
-def do_CCF(ccf_specs, work_path, fits_path, wave=np.arange(3950, 5750, 1)):
+def do_CCF_fits_name(ccf_specs, work_path, fits_name, wave=np.arange(3950, 5750, 1)):
+    """
+
+    Args:
+        ccf_specs: The template CCF spectra (with labels: teff, logg, M/H, alpha/M) to do CCF.
+        work_path: The path of the file of observed spectra.
+        fits_path: the fits name
+        test_size: the size of single CCF
+        wave: the range of wavelength
+
+    Returns:
+        para_CCF: obsid of LAMOST, teff, logg, M/H, alpha/M, snr, rv
+
+    """
+    para_CCF = np.zeros((7), dtype=float)
+    fits = mrs.MrsSpec.from_lrs(work_path + fits_name)
+    test_spec = fits.interp_then_norm(wave, rv=0)[0]
+    snr = fits.snr
+    obsid = fits.obsid
+    maxvalues = []
+    rvs = []
+    for i in ccf_specs['flux_norm_regli_CCF']:
+        rvgrid, ccf = wxcorr_rvgrid(wave, test_spec, CCF_wave, i, rv_grid=np.linspace(-250, 250, 50))
+        maxvalues.append(max(ccf))
+        rvs.append(rvgrid[np.argmax(ccf)])
+
+    para_CCF[0] = int(obsid)
+    para_CCF[1:5] = CCF_specs['p_regli_CCF'][np.argmax(maxvalues)]
+    para_CCF[5] = snr
+    para_CCF[6] = rvs[np.argmax(maxvalues)]
+    # plt.plot(CCF_wave, CCF_specs['flux_norm_regli_CCF'][np.argmax(maxvalues)], 'r-')
+    # plt.plot(wave, test_spec, 'b-')
+    # plt.show()
+    return {'para_CCF': para_CCF}
+
+
+def do_CCF_fits_path(ccf_specs, work_path, fits_path, wave=np.arange(3950, 5750, 1)):
     """
 
     Args:
@@ -69,9 +105,19 @@ if __name__ == '__main__':
     work_path = '/Users/liujunhui/Desktop/2021workMac/202111012totallynew/test_data_group/Test_data_files/'
     input_template_All, input_template_All_Path = getFileName2(work_path, '.gz')
 
+#######do_CCF_fits_path
+    # start = time.time()
+    # results = do_CCF_fits_path(ccf_specs=CCF_specs, work_path=work_path, fits_path=input_template_All[:3],
+    #                  wave=np.arange(3950, 5750, 1))
+    # print(results['para_CCF'])
+    # end = time.time()
+    # print(end - start)
+
+#######do_CCF_fits_name
     start = time.time()
-    results = do_CCF(ccf_specs=CCF_specs, work_path=work_path, fits_path=input_template_All[:3],
-                     wave=np.arange(3950, 5750, 1))
-    print(results['para_CCF'])
-    end = time.time()
-    print(end - start)
+    for i in range(len(input_template_All)):
+        results = do_CCF_fits_name(ccf_specs=CCF_specs, work_path=work_path, fits_name=input_template_All[i],
+                        wave=np.arange(3950, 5750, 1))
+        print(results['para_CCF'])
+        end = time.time()
+        print(end - start)
