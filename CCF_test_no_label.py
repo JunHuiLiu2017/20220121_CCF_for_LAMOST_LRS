1f4b906005332aacdbe2bdfd00a63393768fde82
JunhuiLiu <1948009725@qq.com>
2022-03-12 19:18:53 +0800

    CCF using fits path (fits path is the list of many fits)

--- CCF_test_no_label.py
+++ CCF_test_no_label.py
@@ -2,11 +2,11 @@ import joblib
 import numpy as np
 from laspec.ccf import wxcorr_rvgrid
 import time
-
+import os
 from laspec import mrs
 
 
-def do_CCF(ccf_specs, work_path, fits_path, test_size=1, wave=np.arange(3950, 5750, 1)):
+def do_CCF(ccf_specs, work_path, fits_path, wave=np.arange(3950, 5750, 1)):
     """
 
     Args:
@@ -20,32 +20,44 @@ def do_CCF(ccf_specs, work_path, fits_path, test_size=1, wave=np.arange(3950, 57
         para_CCF: obsid of LAMOST, teff, logg, M/H, alpha/M, snr, rv
 
     """
-
+    test_size = len(fits_path)
     para_CCF = np.zeros((test_size, 7), dtype=float)
 
     for j in range(test_size):
         if j % 1 == 0:
             print(j, time.strftime('%Y_%m_%d_%H_%M_%S', time.localtime(time.time())))
-        fits  = mrs.MrsSpec.from_lrs(work_path + fits_path)
+        fits = mrs.MrsSpec.from_lrs(work_path + fits_path[j])
         test_spec = fits.interp_then_norm(wave, rv=0)[0]
         snr = fits.snr
         obsid = fits.obsid
         maxvalues = []
         rvs = []
         for i in ccf_specs['flux_norm_regli_CCF']:
-            rvgrid, ccf = wxcorr_rvgrid(wave, test_spec, CCF_wave, i, rv_grid=np.linspace(-500, 500, 100))
+            rvgrid, ccf = wxcorr_rvgrid(wave, test_spec, CCF_wave, i, rv_grid=np.linspace(-250, 250, 50))
             maxvalues.append(max(ccf))
             rvs.append(rvgrid[np.argmax(ccf)])
 
-        para_CCF[j][0] = obsid
+        para_CCF[j][0] = int(obsid)
         para_CCF[j][1:5] = CCF_specs['p_regli_CCF'][np.argmax(maxvalues)]
         para_CCF[j][5] = snr
         para_CCF[j][6] = rvs[np.argmax(maxvalues)]
         # plt.plot(CCF_wave, CCF_specs['flux_norm_regli_CCF'][np.argmax(maxvalues)], 'r-')
         # plt.plot(wave, test_spec, 'b-')
         # plt.show()
-
     return {'para_CCF': para_CCF}
+
+
+def getFileName2(path, suffix):
+    input_template_All = []
+    input_template_All_Path = []
+    for root, dirs, files in os.walk(path, topdown=False):
+        for name in files:
+            if os.path.splitext(name)[-1] == suffix:
+                input_template_All.append(name)
+                input_template_All_Path.append(os.path.join(root, name))
+
+    return input_template_All, input_template_All_Path
+
 
 if __name__ == '__main__':
     wave = np.arange(3950, 5750, 1)
@@ -55,7 +67,11 @@ if __name__ == '__main__':
         '/Users/liujunhui/PycharmProjects/20220121_CCF_for_LAMOST_LRS/2022_01_23_23_47_23_imitated_CCF_wl_3800_6000_300_.dump')
 
     work_path = '/Users/liujunhui/Desktop/2021workMac/202111012totallynew/test_data_group/Test_data_files/'
-    fits_path = 'spec-55859-F5902_sp03-232.fits.gz'
-    start = time.time
-    results = do_CCF(ccf_specs=CCF_specs, work_path=work_path, fits_path=fits_path, test_size=1, wave=np.arange(3950, 5750, 1))
-    print(results['para_CCF'][0])
+    input_template_All, input_template_All_Path = getFileName2(work_path, '.gz')
+
+    start = time.time()
+    results = do_CCF(ccf_specs=CCF_specs, work_path=work_path, fits_path=input_template_All[:3],
+                     wave=np.arange(3950, 5750, 1))
+    print(results['para_CCF'])
+    end = time.time()
+    print(end - start)
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # 20220121_CCF_for_LAMOST_LRS
+
+CCF_test_no_labal.py is the program focus on the true spectral file, i.e., XXX.fits.gz
